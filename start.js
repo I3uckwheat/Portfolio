26cc1dafd603dd00248ff734184acea24a0bbd75
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-30 19:21:30 -0600

    sets up start.js

---
+++ start.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config({path: 'variables.env'})
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.error(err.message)
+});
+
+// require("./models/SOMETHING")
+
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running >> PORT ${server.address().port}`);
+});
