5d8ec19a2960ead929c7854a6552a2dcb461c320
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-06 19:37:38 -0600

    adds snake to webpage

--- routes/appsRoutes.js
+++ routes/appsRoutes.js
@@ -1,8 +1,9 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router();
-const appsController = require("../controllers/appsController")
+const appsController = require('../controllers/appsController')
 
-router.get("/", appsController.index)
+router.get('/', appsController.index)
 
+router.use('/snake', express.static('public/apps/Snake-JS'))
 
 module.exports = router
