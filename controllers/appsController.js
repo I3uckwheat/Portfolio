bf95af9f7ae11d977b2712ebef801b3b61e83174
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-06 21:45:57 -0600

    removes debugging statment

--- controllers/appsController.js
+++ controllers/appsController.js
@@ -7,7 +7,6 @@ exports.index = (req, res) => {
 exports.update = (req, res) => {
   const appName = req.params.app
   console.log(`Updating: ${appName}`)
-  console.log(`https://github.com/${process.env.GIT_USER}/${appName}.git`)
   downloadGitRepo(`${process.env.GIT_USER}/${appName}`,
                   `./public/apps/${appName}`,
                   (err) => console.error(err));
