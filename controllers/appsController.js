d00ecafdc005b35cad2f3ff5a51a36b2a8f1ba36
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-06 22:47:58 -0600

    sets up secret for github

--- controllers/appsController.js
+++ controllers/appsController.js
@@ -1,13 +1,19 @@
 const downloadGitRepo = require('download-git-repo');
+const crypto = require('crypto');
 
 exports.index = (req, res) => {
   res.render('apps/appsIndex');
 }
 
+exports.verifyHash = (req, res, next) => {
+  // if(req.body. = crypto.createDecipher('sha1', process.env.GITHUB_SECRET)
+}
+
 exports.update = (req, res) => {
   const appName = req.params.app
   console.log(`Updating: ${appName}`)
-  downloadGitRepo(`${process.env.GIT_USER}/${appName}`,
-                  `./public/apps/${appName}`,
-                  (err) => console.error(err));
+  // downloadGitRepo(`${process.env.GIT_USER}/${appName}`,
+  //                 `./public/apps/${appName}`,
+  //                 (err) => console.error(err));
+  res.status(202).send()
 }
