7cf2dde4742872fea98998ff53be4413298916ff
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-09 00:55:00 -0600

    removes unneeded comments

--- routes/appsRoutes.js
+++ routes/appsRoutes.js
@@ -9,10 +9,9 @@ router.post('/update/:app', appsController.verifyHash, appsController.update);
 router.get('/add', appsController.add);
 
 router.use(express.static('public/apps/'));
-// router.use('/AppName', express.static('public/apps/AppName'));
 
 module.exports = router
 
 
 //TODO
-// add dynamic static routes or add a control panel for adding or deleting them
+// add a control panel for adding or deleting them
