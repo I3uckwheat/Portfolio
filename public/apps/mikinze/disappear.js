80d64c26f156b340a72867cefdc59e57740b6604
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-08 23:26:39 -0600

    removes apps

--- public/apps/mikinze/disappear.js
+++
@@ -1,8 +0,0 @@
-const disappear = document.querySelector("#feather");
-
-disappear.addEventListener("click", removeOwl)
-
-function removeOwl(){
-  disappear.remove();
-  disappear.removeEventListener("click", removeOwl)
-}
