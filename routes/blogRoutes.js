0a45346cec2cc904af255a97f170e9ff775944cb
Briggs Elsperger <briggse@protonmail.com>
2018-06-10 00:26:28 -0500

    adds publications

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,12 +5,11 @@ const blogController = require('../controllers/blogController');
 const authController = require('../controllers/authController');
 
 router.get('/', blogController.index); // Shows all blog posts
-router.get('/update', blogController.downloadPosts, blogController.updatePosts); // REMOVE TEST ROUTE
+router.get('/update', blogController.downloadPosts, blogController.updatePosts); // TODO - REMOVE TEST ROUTE
 router.post('/update',
-            authController.verifyWebhook, // TODO - setup webhook
+            authController.verifyWebhook,
             blogController.downloadPosts,
             blogController.updatePosts); // parses markdown files and updates in DB
 router.get('/:slug', blogController.getPost); // get's post and shows it to client
-
 
 module.exports = router;
