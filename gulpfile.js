dfd0d075906a67857a2eff3cf6a42fd1e44fa8bc
Briggs Elsperger <briggse@protonmail.com>
2018-06-17 19:53:41 -0500

    adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+//TODO - sourcemaps when needed
+
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('default', ['compile-css', 'compress-images']);
+
+gulp.task('compile-css', () => {
+  return gulp.src('./public/stylesheets/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/dist/stylesheets'));
+});
+
+gulp.task('compress-images', () => {
+  return gulp.src('./public/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./public/dist/images'));
+});
