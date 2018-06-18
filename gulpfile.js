44bb1f75553bebaab733543facd865f88dfa0254
Briggs Elsperger <briggse@protonmail.com>
2018-06-18 00:51:41 -0500

    modify gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,37 @@
-//TODO - sourcemaps when needed
-//TODO - setup npm script
-
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const nodemon = require('gulp-nodemon');
+const changed = require('gulp-changed');
 
-gulp.task('default', ['compile-css', 'compress-images']);
+
+gulp.task('default', ['dev']);
+gulp.task('dev', ['compile-css', 'compress-images']); // TODO - add sourcemaps
+gulp.task('prod', ['compile-css', 'compress-images']);
+
+gulp.task('start', () => {
+  const stream = nodemon({
+    script: 'start.js',
+    ext: 'js pug ',
+    tasks: ['dev'],
+    done: 'done'
+  });
+});
 
 gulp.task('compile-css', () => {
-  return gulp.src('./public/stylesheets/*.css')
+  const src = './public/stylesheets/**/*';
+  const dest = './public/dist/stylesheets';
+  return gulp.src(src)
+    .pipe(changed(dest))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./public/dist/stylesheets'));
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('compress-images', () => {
-  return gulp.src('./public/images/**/*')
+  const src = './public/images/**/*';
+  const dest = './public/dist/images';
+  return gulp.src(src)
+    .pipe(changed(dest))
     .pipe(imagemin())
-    .pipe(gulp.dest('./public/dist/images'));
+    .pipe(gulp.dest(dest));
 });
